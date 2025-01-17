3e9277d927d01abf5c28e06f0f25095ef2f57f78
Mennatullah <mennatullah@seamlabs.com>
2025-01-17 12:13:45 +0200

    feat: logo as title based on app language

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,19 @@ import "./Header.css";
 const Header = () => {
   const [isOpened, setIsOpened] = useState(false); // State to track dropdown visibility
   const [language, setLanguage] = useState("English"); // State to track selected language
+  const [logoTitle, setLogoTitle] = useState("Bosta"); 
 
   const toggleDropdown = () => {
     setIsOpened((prev) => !prev); // Toggle the dropdown state
+  };
+  const toggleLogoTitle = (title) => {
+    setLogoTitle(title);
   };
 
   const changeLanguage = (lang) => {
     setLanguage(lang);
     setIsOpened(false); // Close dropdown after selection
+    toggleLogoTitle(lang === "عربي" ? "بوسطة" : "Bosta");
     const direction = lang === "عربي" ? "rtl" : "ltr"; // Set direction based on language
     document.documentElement.setAttribute("dir", direction);  };
 
@@ -20,7 +25,7 @@ const Header = () => {
     <header className="header">
       <div className="right-section">
         <img src={logo} alt="Logo" className="logo" />
-        <span className="company-name">بوسطة</span>
+        <span className="company-name">{logoTitle}</span>
       </div>
       <div className="left-section">
         <div onClick={toggleDropdown} className="dropdown-toggle">
